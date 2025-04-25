b8892bdc01546506a0b9f11c39baae3e5a3b9ba8
thanu1715 <thanudonthula@gmail.com>
2025-04-25 12:50:11 +0000

    Feedback Controller

--- dotnetapp/Services/RoomService.cs
+++ dotnetapp/Services/RoomService.cs
@@ -36,7 +36,7 @@ namespace dotnetapp.Services
         int roomCount = await _context.Rooms.CountAsync(r => r.HotelName == room.HotelName);
         if (roomCount > 10)
         {
-           // throw new RoomException("Total number of rooms for this hotel cannot exceed 10.");
+           throw new RoomException("Total number of rooms for this hotel cannot exceed 10.");
            return false;
         }
  
@@ -56,7 +56,7 @@ namespace dotnetapp.Services
         int roomCount = await _context.Rooms.CountAsync(r => r.HotelName == updatedRoom.HotelName);
         if (roomCount > 10)
         {
-            //throw new RoomException("Total number of rooms for this hotel cannot exceed 10.");
+            throw new RoomException("Total number of rooms for this hotel cannot exceed 10.");
             return false;
         }
  
@@ -76,7 +76,7 @@ namespace dotnetapp.Services
         bool isReferenced = await _context.Bookings.AnyAsync(b => b.RoomId == roomId);
         if (isReferenced)
         {
-            //throw new RoomException("Room cannot be deleted as it is referenced in a booking.");
+            throw new RoomException("Room cannot be deleted as it is referenced in a booking.");
             return false;
         }
  
