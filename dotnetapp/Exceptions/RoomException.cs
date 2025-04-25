69728771908a60d56141f12a06f492ac0bf68d22
thanu1715 <thanudonthula@gmail.com>
2025-04-25 10:57:11 +0000

    roomexception

--- dotnetapp/Exceptions/RoomException.cs
+++ dotnetapp/Exceptions/RoomException.cs
@@ -2,11 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotnetapp.Models;
+using dotnetapp.Controllers;
 
 namespace dotnetapp.Exceptions
 {
-    public class RoomException
+    public class RoomException : Exception
     {
+        public RoomException(string message) : base(message)
+        {
+            
+        }
         
     }
 }
