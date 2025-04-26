c0bf2d2c7d72acf5e1b988229bab5f722c430d39
saikiran2110 <sainandhikonda@gmail.com>
2025-04-26 06:41:56 +0000

    Booking service,controller

--- dotnetapp/Program.cs
+++ dotnetapp/Program.cs
@@ -23,6 +23,7 @@ builder.Configuration
     
     builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
     builder.Services.AddScoped<IAuthService, AuthService>();
+    builder.Services.AddScoped<BookingService>();
     builder.Services.AddAuthentication(options =>
         {
             options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
