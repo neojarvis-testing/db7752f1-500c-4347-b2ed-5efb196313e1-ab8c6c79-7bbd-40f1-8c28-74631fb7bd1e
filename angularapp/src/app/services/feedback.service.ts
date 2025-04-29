c8f74caa5f79239dd80ff5163ab44d086666b222
bharathbrave <bharathbrvae657@gmail.com>
2025-04-29 06:49:01 +0000

    Feedback Service

--- angularapp/src/app/services/feedback.service.ts
+++ angularapp/src/app/services/feedback.service.ts
@@ -1,9 +1,47 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Feedback } from '../models/feedback.model';
+import { Observable,throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { AuthService } from './auth.service';
+import { environment } from 'src/environments/environment';
+import { HttpHeaders } from '@angular/common/http';
+ 
 @Injectable({
   providedIn: 'root'
 })
 export class FeedbackService {
-
-  constructor() { }
+  private apiUrl = environment.apiUrl;
+ 
+  constructor(private http: HttpClient, private authService: AuthService) {}
+  private getHeaders(): HttpHeaders {
+    const token = this.authService.getToken();
+    return new HttpHeaders({ 'Authorization': `Bearer ${token}` });
+  }
+  sendFeedback(feedback: Feedback): Observable<void> {
+    return this.http.post<void>(`${this.apiUrl}/api/feedback`, feedback, { headers: this.getHeaders(), responseType: 'text' as 'json' }).pipe(
+      catchError(this.handleError)
+    );
+  }
+  getAllFeedbacksByUserId(userId: number): Observable<Feedback[]> {
+    return this.http.get<Feedback[]>(`${this.apiUrl}/api/feedback/user/${userId}`, { headers: this.getHeaders() }).pipe(
+      catchError(this.handleError)
+    );
+  }
+ 
+  deleteFeedback(feedbackId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/api/feedback/${feedbackId}`, { headers: this.getHeaders(), responseType: 'text' as 'json'  }).pipe(
+      catchError(this.handleError)
+    );
+  }
+  getFeedbacks(): Observable<Feedback[]> {
+    return this.http.get<Feedback[]>(`${this.apiUrl}/api/feedback`, { headers: this.getHeaders() }).pipe(
+      catchError(this.handleError)
+    );
+  }
+  private handleError(error: any): Observable<never> {
+    console.error('An error occurred:', error);
+    return throwError(error);
+  }
 }
+ 
